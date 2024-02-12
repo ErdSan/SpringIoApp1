578a5b69c0e7d26b4a3942738aee3d4d5ac3bb0a
SanzhiEr <sanzhi.erdyneev@gmail.com>
2024-02-12 23:48:27 +0800

    Добавление DependencyInjection - урок номер 5. Внедрение зависимостей. DI

--- src/main/java/ru/erdyneev/springcourse/TestSpring.java
+++ src/main/java/ru/erdyneev/springcourse/TestSpring.java
@@ -7,7 +7,9 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
+        //musicPlayer.playMusic();
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
     }
